b99bbfce949d59c75e9c3eaf26d2860b8e4590bb
florianldt <florian.ludot@gmail.com>
2020-10-02 21:33:01 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,57 @@
+import fetch from 'node-fetch';
+
+const BASE_URL = 'https://graph.facebook.com/v6.0/me/messages?access_token=';
+const BOT_ACCESS_TOKEN = '';
+const WORPLACE_THREAD_KEY = '';
+
+const renderEmote = (type) => {
+    let emote = '';
+    switch (type) {
+        case 'error':
+            emote = '❌';
+        case 'info':
+            emote = 'ℹ️';
+        case 'success':
+            emote = '✅';
+        case 'warning':
+            emote = '⚠️';
+    }
+    return emote;
+};
+
+const renderText = (title, message) => {
+    let text = '';
+    if (title) text += ' ' + title;
+    if (message) {
+        if (title) text += '\n' + message;
+        else text += ' ' + message;
+    }
+    return text;
+};
+
+export const sendMessage = (type, title, message) => {
+    const emote = renderEmote(type);
+    const text = renderText(title, message);
+    const payload = {
+        recipient: {
+            thread_key: WORPLACE_THREAD_KEY,
+        },
+        message: {
+            text: emote + text,
+        },
+    };
+
+    const config = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(payload),
+    };
+
+    fetch(BASE_URL + BOT_ACCESS_TOKEN, config)
+        .then((data) => {
+            console.log('✅', data);
+        })
+        .catch((e) => console.error(e));
+};
